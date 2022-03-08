69b9d1137b7ade915e6cbdf3b41066c0f22da718
LucienOG <90454957+LucienOG@users.noreply.github.com>
2022-03-08 10:08:30 +0100

    Add files via upload

---
+++ challenge/script.js
@@ -0,0 +1,19 @@
+function visitpage(){     
+window.location='https://github.com/LucienOG/K.O.R-s-Domain.git';
+}
+
+function gamepage(){     
+    window.location='https://github.com/LucienOG/C21-Think-Code-level-1/blob/main/eind%20opdracht/steen-papier-schaar.py';
+}
+
+function htmlpage(){     
+    window.location='https://github.com/LucienOG/K.O.R-s-Domain/blob/master/index.html';
+}
+
+function csspage(){     
+    window.location='https://github.com/LucienOG/K.O.R-s-Domain/blob/master/css/style.css';
+}
+
+function phpPage(){     
+    window.location='https://github.com/LucienOG/C21-Think-Code-level-1/blob/main/eind%20opdracht/steen-papier-schaar.py';
+}
